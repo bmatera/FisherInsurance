d2a21de67dd37e75129568df637b6a5c7189e53c
Bill Matera <matera@optonline.net>
2017-02-10 14:33:28 -0500

    added Claims and Quote classes under Models.

---
+++ src/FisherInsurance/Models/Claims.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FisherInsurance.Models
+{
+    public class Claims
+    {
+        public int Id { get; set; }
+        public string PolicyType { get; set; }
+        public DateTime LossDate { get; set; }
+        public decimal LossAmount { get; set; }
+    }
+}
