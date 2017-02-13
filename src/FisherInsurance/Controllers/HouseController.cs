8c6a0017da9a9c85f764b092d8576ad30045de45
Bill Matera <matera@optonline.net>
2017-02-13 12:50:16 -0500

    Added Model in Quote.cs. Then update quote for Auto, House, Life. Lab 3, exercise 3

--- src/FisherInsurance/Controllers/HouseController.cs
+++ src/FisherInsurance/Controllers/HouseController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using FisherInsurance.Models;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -20,7 +21,15 @@ namespace FisherInsurance.Controllers
         // GET: /house/quote
         public IActionResult Quote()
         {
-            return View();
+            Quote quote = new Quote
+            {
+                Id = 345,
+                Product = "House Insurance",
+                ExpireDate = DateTime.Now.AddDays(45),
+                Price = 45.00M
+
+            };
+            return View(quote);
             //return Ok("This is the /house/quote of the HouseController");
         }
     }
